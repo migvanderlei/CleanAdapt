4bb5f2a6fafc18f49c06ef0874c0e960e0a7683d
Miguel Vanderlei de Oliveira <miguel.vanderlei@itriad.org.br>
2025-05-11 21:04:42 -0400

    chore: pin server params on source only script

--- scripts/source_only_uh.sh
+++ scripts/source_only_uh.sh
@@ -23,19 +23,19 @@ batch_size=48
 
 mkdir -p results
 
-CUDA_VISIBLE_DEVICES=0 python3 source_only_train.py \
+CUDA_VISIBLE_DEVICES=2 python3 source_only_train.py \
 --source_dataset $source_dataset \
 --target_dataset $target_dataset \
 --modality $modality \
 --num_classes 12 \
---adaptation_mode source_only --data_path ./data \
+--adaptation_mode source_only --data_path /home/miguel.oliveira/Workspace/data \
 --num_epochs 40 --lr 0.01 \
 --batch_size $batch_size --milestone 10 20 \
 --weight_decay 1e-7 \
 --pretrained ./models/ \
---num_workers 2 \
+--num_workers 16 \
 --split_path ./splits --save_dir ./results \
---seed 1 \
+--seed $SEED \
 --gpus 1
 
 # clean all __pycache__ and .pyc files
